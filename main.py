123e5fd9874a9fb77598c13ea37e0622ca79c79e
KhresnaPanduI <khresna.pandu10@gmail.com>
2021-10-09 16:48:36 +0700

    create landmark from a webcam. still got an error

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#import libraries
+# import libraries
 import mediapipe as mp
 import cv2
 
@@ -6,14 +6,30 @@ import cv2
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-#Get real time webcam feed
+# Get real time webcam feed
 cap = cv2.VideoCapture(0)
-while cap.isOpened():
-    ret, frame = cap.read()
-    cv2.imshow('Raw Webcam Feed', frame)
+with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+    while cap.isOpened():
+        ret, frame = cap.read()
 
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
+        # Recolor Feed
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Make detections
+        results = hands.process(image)
+
+        # Recolor image back to BGR for rendering
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Right hand
+        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+        # Left Hand
+        # mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+        cv2.imshow('Raw Webcam Feed', frame)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):  # press 'q' to close webcam window
+            break
 
 cap.release()
 cv2.destroyAllWindows()
