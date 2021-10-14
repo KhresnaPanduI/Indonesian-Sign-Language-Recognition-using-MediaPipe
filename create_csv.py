fe00a34e7998a3be1746285fb8f6e9b026242ae6
KhresnaPanduI <khresna.pandu10@gmail.com>
2021-10-14 20:11:29 +0700

    Add initial file for write starter csv.

---
+++ create_csv.py
@@ -0,0 +1,15 @@
+import  csv
+
+# Number of coordinates of each hand
+num_coords = 21
+
+# Write columns name, each have x,y,z coordinate and v for visibility
+landmarks = ['class']
+for val in range(1, num_coords+1):
+    landmarks += ['x{}'.format(val), 'y{}'.format(val), 'z{}'.format(val), 'v{}'.format(val)]
+
+# Write 'landmarks' list to a csv
+with open('coords.csv', mode='w', newline='') as f:
+    csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerow(landmarks)
+
