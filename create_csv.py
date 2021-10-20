0405c759b1a170ee021caf8d1f348578d1c0cf50
KhresnaPanduI <khresna.pandu10@gmail.com>
2021-10-20 11:26:10 +0700

    Create new data with new csv format.

--- create_csv.py
+++ create_csv.py
@@ -9,7 +9,7 @@ for val in range(1, num_coords+1):
     landmarks += ['x{}'.format(val), 'y{}'.format(val), 'z{}'.format(val)]
 
 # Write 'landmarks' list to a csv
-with open('coords.csv', mode='w', newline='') as f:
+with open('2hands.csv', mode='w', newline='') as f:
     csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     csv_writer.writerow(landmarks)
     csv_writer.writerow('change hands')#kolom terakhir untuk membedakan perubahan gestur tangan
