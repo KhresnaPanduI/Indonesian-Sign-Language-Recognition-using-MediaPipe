bbbc027eb726532b54a1aa5c0a6ad6358047320e
KhresnaPanduI <khresna.pandu10@gmail.com>
2022-01-11 17:39:58 +0700

    Remove Gradient boosting and ridge classifier models

--- model.py
+++ model.py
@@ -2,8 +2,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression, RidgeClassifier
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import pickle
 
@@ -17,9 +17,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Create model with sklearn pipeline
 pipelines = {
     'lr': make_pipeline(StandardScaler(), LogisticRegression()),
-    'rc': make_pipeline(StandardScaler(), RidgeClassifier()),
-    'rf': make_pipeline(StandardScaler(), RandomForestClassifier()),
-    'gb': make_pipeline(StandardScaler(), GradientBoostingClassifier())
+    'rf': make_pipeline(StandardScaler(), RandomForestClassifier())
 }
 
 fit_models = {}
@@ -40,5 +38,4 @@ def save_model(name, model):
 
 save_model('RandomForest.pkl', fit_models['rf'])
 save_model('LogisticRegression.pkl', fit_models['lr'])
-save_model('RidgeClassifier.pkl', fit_models['rc'])
-save_model('GradientBoosting.pkl', fit_models['gb'])
+
