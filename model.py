4f6f87aafbb9d54f8a3d00646a2e0ad15df9e525
KhresnaPanduI <khresna.pandu10@gmail.com>
2022-01-11 22:11:39 +0700

    Add gridsearch pipeline for both random forest and logistic regression model. Logistic regression get error TOTAL NO. of ITERATIONS REACHED LIMIT.

--- model.py
+++ model.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
 import pickle
 
@@ -15,11 +16,38 @@ y = df['class']  # label/target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
 
 # Create model with sklearn pipeline
-pipelines = {
-    'lr': make_pipeline(StandardScaler(), LogisticRegression()),
-    'rf': make_pipeline(StandardScaler(), RandomForestClassifier())
-}
+rf_pipe = Pipeline([
+    ('ss', StandardScaler()),
+    ('rf', RandomForestClassifier())
+])
 
+lr_pipe = Pipeline([
+    ('ss', StandardScaler()),
+    ('lr', LogisticRegression())
+])
+
+# Initiaze the hyperparameters for each dictionary
+param_RF = {}
+param_RF['rf__n_estimators'] = [25, 50, 100, 150, 200]
+
+param_LR = {}
+param_LR['lr__max_iter'] = [600, 800, 1000, 1200]
+param_LR['lr__penalty'] = ['l2', 'none']
+
+# Creating Gridsearch for each model
+gs_rf = GridSearchCV(rf_pipe,
+                     param_RF,
+                     cv=5,
+                     verbose=2)
+gs_rf = gs_rf.fit(X_train, y_train)
+
+gs_lr = GridSearchCV(lr_pipe,
+                     param_LR,
+                     cv=5,
+                     verbose=2)
+gs_lr = gs_lr.fit(X_train, y_train)
+
+'''
 fit_models = {}
 for algo, pipeline in pipelines.items():
     model = pipeline.fit(X_train, y_train)
@@ -38,4 +66,4 @@ def save_model(name, model):
 
 save_model('RandomForest.pkl', fit_models['rf'])
 save_model('LogisticRegression.pkl', fit_models['lr'])
-
+'''
--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 import pandas as pd
 df = pd.read_csv('coords.csv')
 print(sum(df['class']=='V'))
+
+dic = {
+    'lr':1,
+    'rf':2
+}
+
+print(dic['lr'])
