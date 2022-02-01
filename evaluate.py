2ff3e0083fac159591bef366977162917a3b09c5
KhresnaPanduI <khresna.pandu10@gmail.com>
2022-02-01 13:30:41 +0700

    Fix visualization to display xticks only tree number

--- evaluate.py
+++ evaluate.py
@@ -1,4 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import json
 
-print('cek')
+df = pd.read_csv('akurasi random forest.csv')
+
+# Change 'rf__n_estimators' to only its value
+for i in range(df.shape[0]):
+    tree = df['param'][i]
+    res = tree.replace("'", "\"")
+    res = json.loads(res)
+    df.at[i,'param'] = res['rf__n_estimators']
+
+print(df['param'])
+
+sns.set()
+ax = sns.lineplot(data=df, x= 'param', y = 'acc')
+ax.set(xlabel='Number of Trees', ylabel='Validation Accuracy')
+plt.show()
+
+
--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 import pandas as pd
+import json
 df = pd.read_csv('coords.csv')
 print(sum(df['class']=='V'))
 
 dic = {
-    'lr':1,
-    'rf':2
+    'lr':1
 }
-
+dicstr = "{'lr':1}"
+print(dicstr)
+print(type(dicstr))
+dicstr = dicstr.replace("'", "\"")
+res = json.loads(dicstr)
+print(res)
+print(type(res))
+print(res['lr'])
+print(dic)
+print(type(dic))
 print('contoh:', dic['lr'])
