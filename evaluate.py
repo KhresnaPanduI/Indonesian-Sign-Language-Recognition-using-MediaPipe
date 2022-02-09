ac1359ea4031a0b0f426dcd6b42272597ad792f5
KhresnaPanduI <khresna.pandu10@gmail.com>
2022-02-09 11:35:40 +0700

    fix confusion matrix on evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,9 +1,9 @@
-import pandas as pd
+from sklearn.metrics import ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 import json
 import joblib
-from sklearn.metrics import confusion_matrix
 
 # Load test data
 X_test = pd.read_csv('X test.csv')
@@ -25,10 +25,10 @@ rf_result = rf_model.score(X_test, y_test)
 # Print accuracy and plot confusion matrix
 print('Random Forest test accuracy: ', rf_result)
 
-rf_matrix = confusion_matrix(rf_model, )
-ConfusionMatrixDisplay.from_estimator(rf_model, X_test, y_test)
-plt.savefig('Random Forest Confusion Matrix.png', bbox_inches='tight')
-plt.show()
+#fig, ax = plt.subplots(figsize=(8,8))
+#cmp = ConfusionMatrixDisplay.from_estimator(rf_model, X_test, y_test, cmap='Blues', ax=ax)
+#plt.savefig('Random Forest Confusion Matrix.png', bbox_inches='tight', dpi=300)
+#plt.show()
 
 # Visualization for validation accuracy
 df = pd.read_csv('akurasi random forest.csv')
@@ -40,15 +40,11 @@ for i in range(df.shape[0]):
     res = json.loads(res)
     df.at[i, 'param'] = res['rf__n_estimators']
 
-print(df['param'])
-
 #sns.set()
 #ax = sns.lineplot(data=df, x='param', y='acc')
 #ax.set(xlabel='Number of Trees', ylabel='Validation Accuracy')
 #plt.show()
 #ax.figure.savefig('Random Forest.png')
-#plt.clf()
-#plt.cla()
 
 '''
 #Logistic Regression Evaluation
@@ -61,8 +57,10 @@ lr_result = lr_model.score(X_test, y_test)
 # Print accuracy and plot confusion matrix
 print('Logistic Regression test accuracy: ', lr_result)
 
-ConfusionMatrixDisplay.from_estimator(lr_model, X_test, y_test)
-plt.show()
+#fig, ax = plt.subplots(figsize=(8,8))
+#cmp = ConfusionMatrixDisplay.from_estimator(lr_model, X_test, y_test, cmap='Blues', ax=ax)
+#plt.savefig('Logistic Regression Confusion Matrix.png', bbox_inches='tight', dpi=300)
+#plt.show()
 
 df_lr = pd.read_csv('akurasi logistic regression.csv')
 
@@ -74,8 +72,7 @@ for i in range(df_lr.shape[0]):
     df_lr.at[i, 'param'] = res['lr__max_iter']
 
 sns.set()
-ax_lr = sns.lineplot(data=df_lr, x= 'param', y = 'acc')
+ax_lr = sns.lineplot(data=df_lr, x='param', y='acc')
 ax_lr.set(xlabel='Maximum Iterations', ylabel='Validation Accuracy')
 plt.show()
 ax_lr.figure.savefig('Logistic Regression.png')
-plt.clf()
